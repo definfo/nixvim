0913a571517731b8309f7367b6bbe00050dc9217
definfo <hjsdbb1@gmail.com>
2025-08-31 23:25:43 +0800

    chore(blink-ripgrep.nvim): migration

--- config/plugins/cmp/blink-cmp.nix
+++ config/plugins/cmp/blink-cmp.nix
@@ -103,12 +103,12 @@ in
             module = "blink-ripgrep";
             name = "Ripgrep";
             opts = {
+              backend.context_size = 5;
+              backend.ripgrep.max_filesize = "1M";
+              backend.ripgrep.project_root_fallback = true;
+              backend.ripgrep.search_casing = "--ignore-case";
               prefix_min_len = 3;
-              context_size = 5;
-              max_filesize = "1M";
               project_root_marker = ".git";
-              project_root_fallback = true;
-              search_casing = "--ignore-case";
               additional_rg_options = { };
               fallback_to_regex_highlighting = true;
               ignore_paths = { };
